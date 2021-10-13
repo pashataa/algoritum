b4f09621c268d90c5571271ca473ee88a1f86154
User 12 <User 12@PC17>
2021-10-13 14:18:02 +0300

    Add project files.

---
+++ algoritum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace algoritum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+        
+            int x;
+
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Всяко x е решение");
+                }
+                else
+                {
+                    Console.WriteLine("Няма решение");
+                }
+            }
+            else
+            {
+                x = -b / a;
+                Console.WriteLine("x е ревно на:" + x );
+            }
+        }
+    }
+}
